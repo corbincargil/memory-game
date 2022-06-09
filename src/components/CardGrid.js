0e96e402434b31997c4cde4771ce0075ba995b19
corbincargil <corbin.cargil@gmail.com>
2022-06-08 19:14:30 -0500

    added 'handleClick function. Need to implement useEffect hook.

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,17 +1,40 @@
 import selectDeck from '../util/selectDeck';
 
 export default function CardGrid(props) {
-    const score = props.score;
-    const round = props.round;
-    const cards = selectDeck(round);
+    let score = props.score;
+    let round = props.round;
+    let cards = selectDeck(round);
+    let prevRound;
+
+    function handleClick(e) {
+        e.currentTarget.value++;
+        if (e.currentTarget.value > 1) {
+            console.log('reset gammmmee');
+        } else {score++;}
+        if (round == 1 && score > 9) { round++;}
+        if (round == 2 && score > 14) {round++;}
+        if (round == 3 && score > 19) {round++;}
+        if (round == 4 && score > 24) {round++;}
+        console.log(score);
+        console.log(round);
+        if (round > prevRound){
+            selectDeck(round);
+            console.log('new deck selected');
+        }
+        prevRound = round;
+        console.log('shuffle and re-render deck here')
+    }
 
     return(
         <div className="card-grid">
             <ul>
             {
                 cards.map((card) => (
-                    <li className='card' key={card.id}>
-                        <img src={card.pic} alt={card.name} />
+                    <li 
+                        className='card' 
+                        key={card.id}
+                        onClick={handleClick}>
+                        <img src={card.pic} alt={card.name}/>
                         <p>{card.name}</p>
                     </li>
                 ))
