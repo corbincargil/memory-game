708f1a9723546b0e9e9600f41c02547c4915540f
corbincargil <corbin.cargil@gmail.com>
2022-06-10 17:22:04 -0500

    cleaned up CardGrid.js component

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,31 +1,20 @@
 import selectDeck from '../util/selectDeck';
 import shuffle from '../util/shuffle';
 import { useState, useEffect } from 'react';
-import{
-    deckOne,
-    deckTwo,
-    deckThree,
-    deckFour,
-    deckFive
-} from '../util/cards';
 
 export default function CardGrid(props) {
     const { hiScore, setHiScore, score, setScore, round, setRound } = props;
     const [cards, setCards] = useState(selectDeck(round));
-    //const [currentCard, setCurrentCard] = useState();
 
-    let prevRound = 1;
     let currentCard;
-    let currentCounter;
-
 
     const resetGame = () => {
-        console.log('reset gammmmee');
         if (score > hiScore) {
             setHiScore(score);
         }
         setScore(0);
         setRound(1);
+        setCards(selectDeck(1));
         cards.map((card) => (card.counter=0));
     }
 
@@ -33,33 +22,24 @@ export default function CardGrid(props) {
         setScore(prevScore => prevScore + 1);
     }
 
-    //need to fix bug where the updated deck and displayed round are delayed
-    const updateRound = () => {
+    const updateRound = (round) => {
         setRound(prevRound => prevRound + 1);
-        setCards((prevCards) => prevCards.concat(...selectDeck(round+1)));
-        // if (round == 1 && score > 5) {
-        //     setRound(prevRound => prevRound+1);
-        //     setCards((prevCards) => prevCards.concat(deckTwo));
-        // }
-        // if (round == 2 && score > 12) {setRound(round+1);}
-        // if (round == 3 && score > 18) {setRound(round+1);}
-        // if (round == 4 && score > 23) {setRound(round+1);}
+        setCards((prevCards) => prevCards.concat(...selectDeck(round)));
     }
 
     const updateCounter = (card) => {
         card.counter++;
     }
 
+
     function handleClick(e) {
+
         cards.forEach((card) => {
             if (card.id == e.currentTarget.id) {
             currentCard = card;
             }
         })
-        console.log(currentCard);
-
-        updateCounter(currentCard);
-
+        currentCard.counter++;
 
         if (currentCard.counter > 1) {
             resetGame();
@@ -67,21 +47,17 @@ export default function CardGrid(props) {
         }
 
         updateScore();
-        
-        
-            //updateRound();
-        
-
-        
-
         setCards(shuffle(cards));
-        console.log(cards);
-        //prevRound = round;
-        //setRound(round);
     }
 
     useEffect(() => {
-        if (round == 1 && score == 5) {updateRound()}
+        if (round == 1 && score == 5) {updateRound(2)}
+        if (round == 2 && score == 10) {updateRound(3)}
+        if (round == 3 && score == 15) {updateRound(4)}
+        if (round == 4 && score == 20) {updateRound(5)}
+        if (round == 5 && score == 25) {updateRound(6)}
+        if (round == 6 && score == 30) {} //Add gameWin modal
+
     })
     
     
